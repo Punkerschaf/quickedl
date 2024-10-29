5cc691ac269d4fdae6ccc783b8b1880094a08a3c
Punkerschaf <105040919+Punkerschaf@users.noreply.github.com>
2024-10-29 06:57:55 +0100

    Update pyinstaller.bash

--- pyinstaller.bash
+++ pyinstaller.bash
@@ -1,1 +1,2 @@
-pyinstaller --onefile --icon ./quickedl_icon_1_mac.icns --name quickedl main.py
+# pyinstaller --onefile --icon ./quickedl_icon_1_mac.icns --name quickedl main.py
+pyinstaller --onefile --name quickedl main.py
