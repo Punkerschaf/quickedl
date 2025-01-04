af90ffe68ae02381266dbe51fe6b5604601dce73
Punkerschaf <punkerschaf@googlemail.com>
2025-01-04 10:41:20 +0100

    docs: random_entry

--- random_entry.py
+++ random_entry.py
@@ -1,15 +1,24 @@
 import random
 
 funny_entries = [
-"Kaffee ist alle",
-"Mein Rechter, Rechter Platz ist frei",
-"Bezahlte Werbung",
-"Unterstützt durch Produktplatzierung",
-"Heute ist ein guter Tag für ein Nickerchen.",
-"404: Eintrag nicht gefunden.",
-"Zufallseintrag erstellt"
+    "Kaffee ist alle",
+    "Mein Rechter, Rechter Platz ist frei",
+    "Bezahlte Werbung",
+    "Unterstützt durch Produktplatzierung",
+    "Heute ist ein guter Tag für ein Nickerchen.",
+    "404: Eintrag nicht gefunden.",
+    "Zufallseintrag erstellt"
 ]
 
 def random_entry(self):
+    """
+    Select a random entry from the list of funny entries.
+
+    Args:
+        self: The instance of the class calling this function.
+
+    Returns:
+        str: A randomly selected entry from the funny_entries list.
+    """
     random_entry = random.choice(funny_entries)
     return random_entry
