b173f5d7975b0a83788373c748038f408d4a6018
Punkerschaf <105040919+Punkerschaf@users.noreply.github.com>
2025-01-29 23:04:03 +0100

    error tolerance

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import subprocess
+from pathlib import Path
 import os
 import logging
 
@@ -6,13 +8,24 @@ def debug(debug, msg):
         print(msg)
 
 def open_directory(path):
-    """Opens a path in a explorer/finder window.
+    """
+    Opens a path in a explorer/finder window.
     Args:
         path: path to open
     """
-    if os.name == 'nt':
-        os.startfile(path)
-    elif os.name == 'posix':
-        os.system(f'open "{path}"')
-    else:
-        logging.error("Unsupported OS")
+    try:
+        directory = Path(path)
+        if not directory.exists():
+            logging.error(f"Directory does not exist: {directory}")
+            return
+        if not directory.is_dir():
+            logging.error(f"Path is not a directory: {directory}")
+            return
+        if os.name == 'nt':
+            subprocess.run(['explorer', str(directory)])
+        elif os.name == 'posix':
+            subprocess.run(['open', str(directory)])
+        else:
+            logging.error("Unsupported OS")
+    except Exception as e:
+        logging.error(f"An error occurred while opening the directory: {e}", exc_info=True)
