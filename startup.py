46f1b93d502a90441fcf3e058c342aa6e21a4abf
Punkerschaf <105040919+Punkerschaf@users.noreply.github.com>
2025-07-23 21:57:04 +0200

    initial function

---
+++ startup.py
@@ -0,0 +1,44 @@
+"""
+This file is part of QuickEDL.
+It creates and handles a message on application startup.
+"""
+
+import logging
+import ttkbootstrap as ttk
+from ttkbootstrap.toast import ToastNotification
+from version import VERSION
+
+class StartupToast:
+    """
+    Class to create a startup message.
+    """
+
+    def __init__(self, **kwargs):
+        self.version = VERSION
+        self.message = f"QuickEDL {self.version}"
+        self.toast = ToastNotification(
+            title="QuickEDL",
+            message=self.message
+            )
+    
+    def addline(self, text):
+        """
+        Add a line to the message.
+        """
+        self.message += f"\n{text}"
+        self.toast.message = self.message
+    
+    def show(self):
+        """
+        Show the toast notification.
+        """
+        self.toast.show_toast()
+        logging.info(f"Startup message: {self.message}")
+
+if __name__ == "__main__":
+    root = ttk.Window(themename="darkly")
+    startup = StartupToast()
+    startup.addline("Das hier ist eine Info.")
+    startup.addline("Und eine andere Information")
+    startup.show()
+    root.mainloop()
