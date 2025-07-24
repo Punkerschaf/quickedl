2f76532155b2b8a2e8d8ae7f8f845246176a53ed
Punkerschaf <105040919+Punkerschaf@users.noreply.github.com>
2025-07-24 16:49:10 +0200

    update version

--- version.py
+++ version.py
@@ -1,11 +1,5 @@
 """
 Version file.
-
-2.2.0 -> 2.2.1
-- Multi line text edit for playlist window
-
-2.2.1 -> 3.0-alpha-cxtest
-- v3 + cx_Freeze build workflow
 """
 
-VERSION = "3.0.0-alpha-cxtest"
+VERSION = "3.0.0-alpha"
