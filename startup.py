2895025a7f9a7b2d10d862c492269ac061ef2a69
Punkerschaf <105040919+Punkerschaf@users.noreply.github.com>
2025-07-24 13:44:10 +0200

    Argument for warning state

--- startup.py
+++ startup.py
@@ -16,25 +16,31 @@ class StartupToast:
     def __init__(self, **kwargs):
         self.version = VERSION
         self.message = f"QuickEDL {self.version}"
-        self.toast = ToastNotification(
-            title="QuickEDL",
-            message=self.message,
-            bootstyle="success",
-            icon="",
-            duration=5000
-            )
+        self.style = "success"
+        self.toast = None
     
-    def addline(self, text):
+    def addline(self, text, warning=False):
         """
         Add a line to the message.
+        Args:
+            text -> String: Text to add to the message
+            warning -> Bool: If True, set style to warning
         """
         self.message += f"\n{text}"
-        self.toast.message = self.message
+        if warning:
+            self.style = "warning"
     
     def show(self):
         """
         Show the toast notification.
         """
+        self.toast = ToastNotification(
+            title="QuickEDL",
+            message=self.message,
+            bootstyle=self.style,
+            icon="",
+            duration=5000
+            )
         self.toast.show_toast()
         logging.info(f"Startup message: {self.message}")
 
@@ -42,6 +48,6 @@ if __name__ == "__main__":
     root = ttk.Window(themename="darkly")
     startup = StartupToast()
     startup.addline("Das hier ist eine Info.")
-    startup.addline("Und eine andere Information")
+    startup.addline("Und eine andere Information", True)
     startup.show()
     root.mainloop()
