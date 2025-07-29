bada14754203d7cdc2a2492c1de2fbb2dfe802c3
Punkerschaf <105040919+Punkerschaf@users.noreply.github.com>
2025-07-29 21:05:18 +0200

    fix: arrow in URL

--- about.py
+++ about.py
@@ -34,8 +34,8 @@ def show_about(app, version):
     label2 = ttk.Label(aboutscreen, text="© 2024-2025, Eric Kirchheim")
     label2.pack()
 
-    githuburl = f"↗{GITHUBURL}"
-    label3 = ttk.Label(aboutscreen, text=githuburl, bootstyle="info")
+    githuburl = GITHUBURL
+    label3 = ttk.Label(aboutscreen, text=f"↗ {githuburl}", bootstyle="info")
     label3.configure(underline=True)
     label3.pack(padx=10, pady=10)
     label3.bind("<Button-1>", lambda e: open_in_browser(githuburl))
